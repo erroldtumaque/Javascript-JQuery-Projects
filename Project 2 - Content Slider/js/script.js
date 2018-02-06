05eeb4f4a426c2cc3221af5d2c6b91135a0eb031
erroldtumaque <errold.tumaque@gmail.com>
2018-02-05 20:57:35 -0800

    Project 2 - Content Slider
    Covers Semantic HTM5 Mark up
    CSS positioning
    jQuery Selectors
    jQuery Methods
    fadeIn & fadeOut
    setInterval Function

---
+++ Project 2 - Content Slider/js/script.js
@@ -0,0 +1,56 @@
+ $(document).ready(function() {
+     //Set Options
+     var speed = 500; //fade speed
+     var autoswitch = true; // auto slider option
+     var autoswitch_speed = 4000 // auto slider speed
+
+     //Add initial active class
+     $('.slide').first().addClass('active');
+
+     //Hide all slides
+     $('.slide').hide();
+
+     //Show first sldie
+     $('.active').show();
+
+     // Next Handler
+     $('#next').on('click', nextSlide);
+
+     // Prev Handlers
+     $('#prev').on('click', prevSlide);
+
+
+     if (autoswitch == true) {
+         setInterval(nextSlide, autoswitch_speed)
+     }
+
+     // Switch to next slide 
+     function nextSlide() {
+         $('.active').removeClass('active').addClass('oldActive');
+         if ($('.oldActive').is(':last-child')) {
+             $('.slide').first().addClass('active');
+         } else {
+             $('.oldActive').next().addClass('active');
+         }
+
+         $('.oldActive').removeClass('oldActive');
+         $('.slide').fadeOut(speed);
+         $('.active').fadeIn(speed);
+     }
+
+     // Switch to previous slide 
+     function prevSlide() {
+
+         $('.active').removeClass('active').addClass('oldActive');
+         if ($('.oldActive').is(':first-child')) {
+             $('.slide').last().addClass('active');
+         } else {
+             $('.oldActive').prev().addClass('active');
+         }
+
+         $('.oldActive').removeClass('oldActive');
+         $('.slide').fadeOut(speed);
+         $('.active').fadeIn(speed);
+     }
+
+ });
